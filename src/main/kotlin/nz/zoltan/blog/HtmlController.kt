70e5bff2ed123199a3dd197fc79092ee91b618e0
Zoltan <zdebre@gmail.com>
2019-01-31 14:42:00 +1300

    Add basic  mustache templates

---
+++ src/main/kotlin/nz/zoltan/blog/HtmlController.kt
@@ -0,0 +1,15 @@
+package nz.zoltan.blog
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        model["title"] = "Blog"
+        return "blog"
+    }
+}
