cea8fa681e77b15546c4f19a15442f7a57f758f2
Zoltan <zdebre@gmail.com>
2019-02-04 11:08:00 +1300

    Add JUnit5 tests

--- src/test/kotlin/nz/zoltan/blog/BlogApplicationTests.kt
+++ src/test/kotlin/nz/zoltan/blog/BlogApplicationTests.kt
@@ -1,16 +1,12 @@
 package nz.zoltan.blog
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 class BlogApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
