03015f8d541066d3a3a110d7fd83cb40865a9982
Zoltan <zdebre@gmail.com>
2019-02-11 14:46:00 +1300

    Extend HtmlController with article and with properties

--- src/main/kotlin/nz/zoltan/blog/BlogApplication.kt
+++ src/main/kotlin/nz/zoltan/blog/BlogApplication.kt
@@ -2,13 +2,15 @@ package nz.zoltan.blog
 
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class BlogApplication
 
 fun main(args: Array<String>) {
-	runApplication<BlogApplication>(*args) {
-		setBannerMode(Banner.Mode.OFF)
-	}
+    runApplication<BlogApplication>(*args) {
+        setBannerMode(Banner.Mode.OFF)
+    }
 }
---
+++ src/main/kotlin/nz/zoltan/blog/BlogProperties.kt
@@ -0,0 +1,14 @@
+package nz.zoltan.blog
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("blog")
+class BlogProperties {
+    lateinit var title: String
+    val banner = Banner()
+
+    class Banner {
+        var title: String? = null
+        lateinit var content: String
+    }
+}
