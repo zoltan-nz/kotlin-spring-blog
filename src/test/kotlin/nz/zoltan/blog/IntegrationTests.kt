7b8e9e95743c1218c5975b2438cf2bfec7360e95
Zoltan <zdebre@gmail.com>
2019-02-13 09:46:00 +1300

    Use Jetty instead of Tomcat

--- src/test/kotlin/nz/zoltan/blog/IntegrationTests.kt
+++ src/test/kotlin/nz/zoltan/blog/IntegrationTests.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,14 +21,18 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
     @Test
     fun `Assert blog page title, content and status code`() {
         println(">> Assert blog page title, content and status code")
-        val entity = restTemplate.getForEntity("/", String::class.java)
+        val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>", "Reactor")
     }
 
     @Test
     fun `Assert article page title, content and status code`() {
-        println(">> TODO")
+        println(">> Assert article page title, content and status code")
+        val title = "Reactor Aluminium has landed"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(title, "Lorem ipsum", "dolor sit amet")
     }
 
     @AfterAll
